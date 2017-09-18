4859ba4d97622b0582e03f531ac9781aa98e7ca8
Karl Stolley <karl.stolley@gmail.com>
2017-09-18 17:46:58 -0500

    Clean up HTML; simplify record loop

--- site.js
+++ site.js
@@ -1,61 +1,47 @@
-var createTable = function(){
-	'use strict';
+var  users = [{
+		"name":"Qi Zhang",
+		"gitname": "QiZhang79",
+			"id": "001",
+			"location": "Chicago",
+			"social_media":"facebook"
+	},{
+		"name":"JunFeng Wang",
+		"gitname": "jwang206",
+			"id": "002",
+			"location": "Chicago",
+			"social_media":"instagram"
+	},{
+		"name":"Sara Lopez",
+		"gitname": "slopez12",
+			"id": "003",
+			"location": "Chicago",
+			"social_media":"instagram"
+	},{
+		"name":"Meiqi Zhao",
+		"gitname": "mzhao22",
+			"id": "004",
+			"location": "Chicago",
+			"social_media":"facebook"
+	}
+];
 
-	var  users = [{
-			"name":"Qi Zhang",
-			"gitname": "QiZhang79",
-		    "id": "001",
-		    "location": "Chicago",
-		    "social_media":"facebook"
-		},{
-			"name":"JunFeng Wang",
-			"gitname": "jwang206",
-		    "id": "002",
-		    "location": "Chicago",
-		    "social_media":"instagram"
-		},{
-			"name":"Sara Lopez",
-			"gitname": "slopez12",
-		    "id": "003",
-		    "location": "Chicago",
-		    "social_media":"instagram"
-		},{
-			"name":"Meiqi Zhao",
-			"gitname": "mzhao22",
-		    "id": "004",
-		    "location": "Chicago",
-		    "social_media":"facebook"
-		}
-	];
-
-
-
-	$('#result').hide();
 	$('#button').on("click",function(e){
-		$('#result').show();
 		var container = ["name", "gitname", "id", "loaction"];
+		var tbody = $("#user_table tbody");
 
 		for(var i = 0; i < users.length; i++){
-			var $row = '<tr>';
-			var tbody = $("#user_table");
-
+			var row = '<tr>';
 			// for(var j = 0; j < 4; j++ ){
 			// 	$row += "<td>" + users[i]["name"] + "</td>";
 
 			// }
-			$row += "<td>" + users[i]["name"] + "</td>";
-			$row += '<td><a href="https://github.com/'
+			row += "<td>" + users[i]["name"] + "</td>";
+			row += '<td><a href="https://github.com/'
   				+ users[i]["gitname"] + '">' + users[i]["gitname"] + '</a></td>'
-			$row += "<td>" + users[i]["id"] + "</td>";
-			$row += "<td>" + users[i]["location"] + "</td>";
-			$row += "<td>" + users[i]["social_media"] + "</td>";
-
-
-			$row += "</tr>";
-			tbody.append($row);
+			row += "<td>" + users[i]["id"] + "</td>";
+			row += "<td>" + users[i]["location"] + "</td>";
+			row += "<td>" + users[i]["social_media"] + "</td>";
+			row += "</tr>";
+			tbody.append(row);
 		}
-
 	});
-};
-
-$(document).ready(createTable);
